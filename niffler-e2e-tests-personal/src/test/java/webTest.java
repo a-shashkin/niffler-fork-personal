97ea06db069cb9f8daf127f99650c09f45559630
a-shashkin <caezar1993@gmail.com>
2025-05-28 21:38:09 +0400

    First commit - made first Selenide test on localhost-run Niffler

---
+++ niffler-e2e-tests-personal/src/test/java/webTest.java
@@ -0,0 +1,18 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class webTest {
+
+    @Test
+    void mainPageMustShowAfterLoginTest() {
+        open("http://127.0.0.1:9000/login");
+        $("input[name='username']").setValue("Kaiser");
+        $("input[name='password'").setValue("DarkKaiser93");
+        $("button[type='submit']").click();
+        $(".MuiTypography-root.MuiTypography-h5.css-126ce46")
+                .shouldBe(Condition.visible);
+    }
+}
